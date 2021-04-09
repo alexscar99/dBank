95e90338997ad60e754bfc87a41d387dd3394133
Alex <alex.scarlett91@gmail.com>
2021-04-09 15:17:37 -0400

    Wasn't going to put on GitHub, so project is already in progress
    Start work on Token contract, specifically mint function

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -4,24 +4,17 @@ pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
+    // add minter variable
     address public minter;
 
-    event MinterChanged(address indexed from, address to);
+    // add minter changed event
 
     constructor() public payable ERC20("Decentralized Bank Currency", "DBC") {
+        // assign initial minter
         minter = msg.sender;
     }
 
-    function passMinterRole(address dBank) public returns (bool) {
-        require(
-            msg.sender == minter,
-            "Error, only owner can change pass minter role"
-        );
-        minter = dBank;
-
-        emit MinterChanged(msg.sender, dBank);
-        return true;
-    }
+    // add pass minter role function
 
     function mint(address account, uint256 amount) public {
         require(
